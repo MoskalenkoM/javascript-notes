54f4af53a9b9b4fa3249d21e147f236ad937d722
Maxim Moskalenko <maksm.84@gmail.com>
2019-12-17 15:07:26 +0300

    Dynamic typing

---
+++ DynamicTyping.js
@@ -0,0 +1,21 @@
+// Example dynamic typing.
+// Примеры динамической типизации.
+
+let a; // неявное присваивание значения undefined
+console.log(`Value: ${a}, Type:`, typeof a); // undefined
+
+a = 1; // переменная связывается с типом в момент присваивания значения
+console.log(`Value: ${a}, Type:`, typeof a); // number
+
+a = 'Simple string';
+console.log(`Value: ${a}, Type:`, typeof a); // string
+
+a = true;
+console.log(`Value: ${a}, Type:`, typeof a); // boolean
+
+a = {};
+console.log(`Value: ${a}, Type:`, typeof a); // object
+
+a = function() {};
+console.log(`Value: ${a}, Type:`, typeof a); // function
+// тип function - это ошибка в языке, на самом деле функции в JavaScript - это объекты
